4f35c4acbe5eb638d7d30ddca74a0cfc7af34a47
gngsn <rudtjs4540@gmail.com>
2022-02-01 13:08:23 +0900

    update: Composite Pattern

--- design-pattern/composite/files/Client.java
+++ design-pattern/composite/files/Client.java
@@ -11,33 +11,29 @@ public class Client {
         File assign1 = new File("picture", 256);
         folder1.add(assign1);
 
+
 //        2 학년
         Folder folder2 = new Folder("sophomore");
         rootFolder.add(folder2);
 
-
-//        2 학년 1 학기
+//      2 학년 1 학기
         Folder sem1Folder = new Folder("1semester");
-
         Folder lecture1 = new Folder("C programming");
+        File lecture2 = new File("Python", 520);
 
         File assign3 = new File("array", 266);
         File assign4 = new File("pointer", 302);
-        lecture1.add(assign3);
-        lecture1.add(assign4);
 
         sem1Folder.add(lecture1);
-
-        File lecture2 = new File("Python", 520);
-        lecture1.add(lecture2);
+        sem1Folder.add(lecture2);
+        lecture1.add(assign3);
+        lecture1.add(assign4);
 
 //        2 학년 2 학기
         Folder sem2Folder = new Folder("2semester");
 
-
         folder2.add(sem1Folder);
         folder2.add(sem2Folder);
-
 
         rootFolder.getSize();
     }
