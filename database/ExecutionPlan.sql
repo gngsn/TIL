e304247295ee7c6690247e757d44b513f9cc4f23
gngsn <rudtjs4540@gmail.com>
2022-03-19 21:23:37 +0900

    sql: Execution Plan

--- database/ExecutionPlan.sql
+++ database/ExecutionPlan.sql
@@ -43,5 +43,49 @@ EXPLAIN SELECT
 EXPLAIN
 SELECT emp.emp_no, sal.salary
 FROM employees as emp,
-    (SELECT emp_no as emp_no, salary FROM salaries WHERE emp_no BETWEEN 10001 AND 20000) as sal
-WHERE emp.emp_no = sal.emp_no;
+    (SELECT MAX(salary) as salary 
+    FROM salaries 
+    WHERE emp_no BETWEEN 10001 AND 20000
+    ) as sal;
+
+
+-- select_type : UNION RESULT
+EXPLAIN
+SELECT all_emp.*
+FROM (
+	SELECT hire_date FROM employees emp WHERE gender = 'M'
+	UNION 
+	SELECT hire_date FROM employees emp WHERE gender = 'M'
+) as all_emp;
+
+
+-- select_type : DEPENDENT SUBQUERY
+EXPLAIN
+SELECT manager.dept_no, (
+	SELECT emp.first_name 
+    FROM employees emp 
+    WHERE gender = 'F' AND emp.emp_no = manager.emp_no
+) as name
+FROM dept_manager as manager;
+
+
+-- select_type : DEPENDENT UNION
+EXPLAIN
+SELECT manager.dept_no, (
+	SELECT emp.first_name FROM employees emp WHERE gender = 'F' AND emp.emp_no = manager.emp_no
+	UNION 
+	SELECT emp.first_name FROM employees emp WHERE gender = 'M' AND emp.emp_no = manager.emp_no
+) as name
+FROM dept_manager as manager;
+
+
+-- select_type : UNCACHABLE SUNQUERY
+EXPLAIN SELECT * FROM employees
+WHERE emp_no = (SELECT ROUND(RAND()*1000000));
+
+
+-- select_type : MATERIALIZED
+EXPLAIN SELECT * FROM employees
+WHERE emp_no IN (SELECT emp_no FROM salaries WHERE from_date > '2020-01-01' AND salary > 200000);
+
+
