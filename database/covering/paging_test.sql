a04e3e3f56babd005bcdab64999daa099cccbeca
gngsn <rudtjs4540@gmail.com>
2022-07-31 14:34:26 +0900

    update: covering index test

--- database/covering/paging_test.sql
+++ database/covering/paging_test.sql
@@ -27,7 +27,41 @@ SELECT * FROM employee WHERE first_name = 'Parto' LIMIT 13000, 1000;
 1000 rows in set (3.62 sec)
 */
 
+SELECT * FROM employee
+WHERE first_name = 'Parto'
+ORDER BY emp_no DESC
+LIMIT 13000, 1000;
+-- 1000 rows in set (1.45 sec)
 
 
+SELECT * FROM employee 
+WHERE emp_no < 13000 
+AND first_name = 'Parto'
+ORDER BY emp_no DESC
+LIMIT 1000;
+/*
+mysql> SELECT * FROM employee 
+    -> WHERE first_name = 'Parto' AND emp_no > 13000 
+    -> ORDER BY emp_no DESC LIMIT 1000;
+mysql> SELECT * FROM employee  WHERE emp_no > 13000 AND first_name = 'Parto' LIMIT 1000;
++---------+------------+------------+-----------------+--------+------------+
+| emp_no  | birth_date | first_name | last_name       | gender | hire_date  |
++---------+------------+------------+-----------------+--------+------------+
+|   16964 | 1957-05-10 | Parto      | Bernini         | M      | 1992-09-27 |
+|   17458 | 1957-06-10 | Parto      | Auyong          | M      | 1985-11-18 |
+|   19026 | 1953-03-18 | Parto      | Byoun           | F      | 1985-06-12 |
+			   			   			...
++---------+------------+------------+-----------------+--------+------------+
+1000 rows in set (0.02 sec)
+*/
+
+
+
+EXPLAIN SELECT e.* FROM employee as e JOIN (
+	SELECT emp_no
+	FROM employee 
+	WHERE first_name = 'Parto'
+	LIMIT 13000, 1000) as cover
+ON e.emp_no = cover.emp_no;
 
 
