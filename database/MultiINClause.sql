481ebd21305d71302a33760bf786f3fdd25e7274
gngsn <rudtjs4540@gmail.com>
2022-08-19 20:31:12 +0900

    mysql: multi IN Clause Test query

---
+++ database/MultiINClause.sql
@@ -0,0 +1,18 @@
+SELECT * FROM test.multi_in_clause;
+
+truncate test.multi_in_clause;
+DROP PROCEDURE IF EXISTS save_data;
+DELIMITER $$
+CREATE PROCEDURE save_data()
+BEGIN
+	DECLARE i INT DEFAULT 1;
+    WHILE(i <= 10000) DO
+        INSERT INTO test.multi_in_clause (x1, y1, x2, y2) VALUES (
+			FLOOR(RAND() * 1000), FLOOR(RAND() * 1000), FLOOR(RAND() * 1000), FLOOR(RAND() * 1000)
+		);
+        SET i = i + 1;
+    END WHILE;
+END$$
+DELIMITER ;
+
+CALL save_data();
