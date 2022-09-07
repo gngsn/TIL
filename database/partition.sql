49aa5955a37ceb447413f462f9985937e5a1f432
gngsn <rudtjs4540@gmail.com>
2022-09-07 22:02:47 +0900

    update: create parition

--- database/partition.sql
+++ database/partition.sql
@@ -31,7 +31,7 @@ CREATE TABLE mirrorline.users_after (
     -- 데이터가 존재할 때에도 파티션 적용이 가능할까? - YES
 INSERT INTO users_after(email, name, age, reg_at) VALUES('gngsn@gmail.com', 'gngsn', 25, '2022-08-23 00:00:00');
 
--- 생성한 테이블에 ALTER로 파티션 적용
+	-- 생성한 테이블에 ALTER로 파티션 적용
 ALTER TABLE users_after PARTITION BY RANGE (TO_DAYS(reg_at)) (
 	PARTITION P_202206 VALUES LESS THAN (20220701),
 	PARTITION P_202207 VALUES LESS THAN (20220801), 
@@ -39,18 +39,24 @@ ALTER TABLE users_after PARTITION BY RANGE (TO_DAYS(reg_at)) (
 	PARTITION P_maxvalue VALUES LESS THAN MAXVALUE
 );
 
-
 -- 생성된 파티션 확인 
 SELECT PARTITION_NAME,TABLE_ROWS
 FROM INFORMATION_SCHEMA.PARTITIONS
-WHERE TABLE_NAME = 'rc1';
+WHERE TABLE_NAME = 'users';
 
 
+
+-- ========== INSERT INTO PARTITION ========== -----
+
+-- 
 ALTER TABLE mirrorline.users
 ADD COLUMN `email` VARCHAR(100) NOT NULL AFTER `user_id`,
 ADD UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE;
-;
 
-select count(*) from users;
 
-select TO_DAYS('202207');
+
+
+
+
+
+
