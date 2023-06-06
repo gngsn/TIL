7f296453c8839bf56cffadcbbc135bf391f6487f
gngsn <rudtjs4540@gmail.com>
2023-06-06 22:10:16 +0900

    update: changes jdk

---
+++ Java/changesJdkRelease/JDK12CompactNumber.java
@@ -0,0 +1,32 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class JDK12CompactNumber {
+    public static void main(String[] args) {
+        enLocaleShort();
+        krLocaleShort();
+        enLocaleLong();
+    }
+
+    private static void enLocaleShort() {
+        NumberFormat fmt = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
+        System.out.println(fmt.format(1_000));
+        System.out.println(fmt.format(10_000));
+        System.out.println(fmt.format(1_000_000));
+    }
+
+    private static void krLocaleShort() {
+        NumberFormat fmt = NumberFormat.getCompactNumberInstance(Locale.KOREA, NumberFormat.Style.SHORT);
+
+        System.out.println(fmt.format(1_000));
+        System.out.println(fmt.format(10_000));
+        System.out.println(fmt.format(1_000_000));
+    }
+
+    private static void enLocaleLong() {
+        NumberFormat fmt = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.LONG);
+        System.out.println(fmt.format(1_000));
+        System.out.println(fmt.format(10_000));
+        System.out.println(fmt.format(1_000_000));
+    }
+}
---
+++ Java/changesJdkRelease/TextBlocks.java
@@ -0,0 +1,46 @@
+class TextBlocks {
+    public static void main(String[] args) {
+        oldStyle();
+        jsonBlock();
+        emptyBlock();
+        escapeSequence();
+    }
+
+    private static void oldStyle() {
+        String text = "{\n" +
+                "  \"name\": \"John Doe\",\n" +
+                "  \"age\": 45,\n" +
+                "  \"address\": \"Doe Street, 23, Java Town\"\n" +
+                "}";
+        System.out.println(text);
+    }
+
+    private static void jsonBlock() {
+        String text = """
+            {
+              "name": "John Doe",
+              "age": 45,
+              "address": "Doe Street, 23, Java Town"
+            }
+    """;
+        System.out.println(text);
+    }
+
+    private static void emptyBlock() {
+        String text = """
+                """;
+     System.out.println("||" + text + "||");
+    }
+
+    private static void escapeSequence() {
+        String text1 = """
+                abc\
+                def\
+                ghi\
+                """;
+        String text2 = """
+                abc\sdef\sghi""";
+        System.out.println("||" + text1 + "||"); // ||abcdefghi||
+        System.out.println("||" + text2 + "||"); // ||abc def ghi||
+    }
+}
